b6487bcf3f38d00e5943201ef13930022d50bf48
Angelo Corsaro <angelo@icorsaro.net>
2018-07-13 00:17:18 +0200

    added some more traces in example

--- example/actor/aecho.ml
+++ example/actor/aecho.ml
@@ -59,12 +59,16 @@ let () =
         from <?> (Some self,`EchoX (msg, grow)) >>= continue self state
     | `GetCount ->         
         from <?> (Some self, `Count (safe_get state)) >>= continue self state        
-    | _ -> (Lwt.return_false >>= terminate self state)) in
+    | _ -> 
+      Lwt.ignore_result @@ Lwt_io.printf "--- > echo_stateful_actor: Received unexpexted message -- TERMINATING < ---- \n" 
+      ; (Lwt.return_false >>= terminate self state)) in
   
   let echo_actor = spawn (fun self state from -> function 
     | `Msg msg ->  from <?> (Some self,`Echo msg) >>= continue self state 
     | `MsgX (msg, grow) -> from <?> (Some self,`EchoX (msg, grow)) >>= continue self state 
-    | _ -> Lwt.return_true >>= continue self state ) in 
+    | _ -> 
+      Lwt.ignore_result @@ Lwt_io.printf "--- > echo_actor: Received unexpexted message -- SKIPPING < ---- \n" 
+      ; Lwt.return_true >>= continue self state ) in 
 
   let periodic_actor = spawn ~timeout:(Some 0.1) (fun self state from -> function 
     | `EchoX (msg, grow) -> 
@@ -74,7 +78,9 @@ let () =
       let state = apppend_state state (match String.length msg with 
       | 1 ->  (Printf.sprintf "%s%c" msg c, true, from) 
         
-      | 10 as len -> (String.sub msg 1 (len-1), false, from)         
+      | 10 as len -> 
+        let _ = from <?> (Some self, `GetCount) in
+        (String.sub msg 1 (len-1), false, from)         
 
       | len ->  (match grow with 
         | true ->  (Printf.sprintf "%s%c" msg c, true, from)
@@ -93,7 +99,9 @@ let () =
 
       | None -> continue self (Some []) true )
     | `Terminate -> terminate self state false
-    | _ -> Lwt.return_true >>= continue self state) in 
+    | _ -> 
+      Lwt.ignore_result @@ Lwt_io.printf "--- > periodic_actor: Received unexpexted message -- SKIPPING < ---- \n" 
+      ; Lwt.return_true >>= continue self state) in 
 
   
   let client_actor = receive (fun self _ -> function
