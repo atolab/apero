337a516b14aa0cd7cfa228012bffcf12db6b6ec9
Angelo Corsaro <angelo@icorsaro.net>
2018-07-14 23:08:49 +0200

    Some rework in the implementation and type bounds

--- src/core/actor.ml
+++ src/core/actor.ml
@@ -1,53 +1,91 @@
 open Lwt.Infix
 
-module Actor = struct
+module ActorCoreMessage = struct 
+  type core_message = [ `Timeout of float  | `Terminate | `EmptyMessage ]  [@@deriving show]
+end 
 
-    type 'a core_message = [`ActorMessage of 'a | `Timeout of float  | `Terminate | `EmptyMessage ]    
+module Actor : sig 
+    
+
+    type 'msg actor_mailbox
+    type  'msg t  
+    type ('msg, 's) reaction = ([> ActorCoreMessage.core_message ] as 'msg) t -> 's option -> 'msg actor_mailbox  option -> 'msg -> ('msg t * 's option * bool) Lwt.t 
+    
+    val addr :  ([> ActorCoreMessage.core_message ] as 'msg) t -> 'msg actor_mailbox
+
+    val spawn : ?queue_len : int -> ?state : 's option -> ?timeout :  float option -> ('msg, 's) reaction -> ('msg actor_mailbox * unit Lwt.t)
+    val set_timeout : ([> ActorCoreMessage.core_message ] as 'msg) t -> float option -> 'msg t
+    val get_timeout : ([> ActorCoreMessage.core_message ] as 'msg) t -> float option
+    
+    val terminate : ([> ActorCoreMessage.core_message ] as 'msg) t -> 's option ->  bool ->  ('msg t * 's option * bool) Lwt.t
+    val continue : ([> ActorCoreMessage.core_message ] as 'msg) t -> 's option -> bool ->  ('msg t * 's option * bool) Lwt.t
+    
+    val (<!>) :  ([> ActorCoreMessage.core_message ] as 'msg) actor_mailbox -> ('msg actor_mailbox option * 'msg) -> unit Lwt.t
+    val send : ([> ActorCoreMessage.core_message ] as 'msg) actor_mailbox -> 'msg actor_mailbox option ->  'msg -> unit Lwt.t
+         
+    val maybe_send : ([> ActorCoreMessage.core_message ] as 'msg) actor_mailbox option -> 'msg actor_mailbox option ->  'msg -> bool Lwt.t
+      
+    val (<?!>) : ([> ActorCoreMessage.core_message ] as 'msg) actor_mailbox option -> ('msg actor_mailbox option *  'msg) -> bool Lwt.t
+        
+end = struct
+    
+    
     module EventStream = Event_stream.EventStream.Make(Stream_lwt.Stream)
     module ActorId = Id.Make(Int64)
     
-    type  'a t = 
-      { self_sink : 'a EventStream.Sink.s
-      ; self_source : 'a EventStream.Source.s
+
+    type 'msg actor_mailbox = ActorMailbox of 
+      { inbox : ('msg actor_mailbox option * 'msg) EventStream.Source.s
+      ; outbox:  ('msg actor_mailbox option * 'msg) EventStream.Sink.s }
+
+    type  'msg t = 
+      { mailbox : 'msg actor_mailbox  
       ; timeout : float option              
       ; run_loop : unit Lwt.t
       ; completer : unit Lwt.u 
       ; aid : ActorId.t} 
       
-    let spawn ?(queue_len=256) ?(state=None) ?(timeout=None) handler =       
-      let (self_source, self_sink) = EventStream.create queue_len in       
+    type ('msg, 's) reaction = ([> ActorCoreMessage.core_message ] as 'msg) t -> 's option -> 'msg actor_mailbox  option -> 'msg -> ( 'msg t * 's option * bool) Lwt.t       
+
+    let addr actor = actor.mailbox
+
+    let spawn ?(queue_len=256) ?(state=None) ?(timeout=None) (handler : ('msg, 's) reaction) =                         
+     
+      let (inbox, outbox) = EventStream.create queue_len in
+      let mailbox = ActorMailbox { inbox ; outbox } in 
       let (run_loop, completer) = Lwt.task () in     
-      let self = { 
-          self_sink
-        ; self_source
+      let self = 
+        { mailbox
+        ; timeout
         ; run_loop
         ; completer
-        ; timeout 
-        ; aid = ActorId.next_id () } in    
-      let rec loop (self, state, continue) = 
+        ; aid = ActorId.next_id () } in
+        
+      let rec loop (handler : ('msg, 's) reaction) (self, state, continue)  = 
         match continue with
         | true -> 
-          (let ps = (EventStream.Source.get self_source >|= function  Some v ->  v | None -> `EmptyMessage) :: 
+          (let ps = (EventStream.Source.get inbox >|= function  Some v ->  `ActorMessage v | None -> `EmptyMessage) :: 
           (match self.timeout with 
             | Some period ->  [ Lwt_unix.sleep period >|= fun () -> `Timeout period ]        
             | None -> []) in     
 
           match%lwt Lwt.pick ps with 
-          | `ActorMessage (from,  msg) -> (handler self state from msg) >>= loop
-          | `Timeout period  -> (handler self state None (`Timeout period)) >>= loop      
-          | `EmptyMessage -> loop (self, state, false)
+          | `ActorMessage (from,  msg) -> (handler self state from msg) >>= (loop handler)
+          | `Timeout period  -> (handler self state None (`Timeout period)) >>= (loop handler)      
+          | `EmptyMessage -> loop handler (self, state, false)
           | `Terminate -> (handler self state None `Terminate) >|=  fun _ -> (self, true))
         | false -> Lwt.return (self, false)
-      in Lwt.async (fun () -> loop (self, state, true))
-      ; self
+      in Lwt.async (fun () -> loop handler (self, state, true) )
+      ; (self.mailbox, run_loop)
     
-    let set_timeout (a : 'a t)  timeout = {a with timeout}
-    let get_timeout (a : 'a t)  = a.timeout
+    let set_timeout a  timeout = {a with timeout}
+    let get_timeout a  = a.timeout
 
-    let terminate (a : 'a t) (state : 's) _ = Lwt.return (a, state, false)
-    let continue (a : 'a t) (state : 's) _ = Lwt.return (a, state, true)
+    let terminate (a : ([> ActorCoreMessage.core_message ] as 'msg) t) (state: 's option)  (_ : bool) = Lwt.return (a, state, false)
+    let continue (a : ([> ActorCoreMessage.core_message ] as 'msg) t) (state: 's option)  (_:bool) = Lwt.return (a, state, true)
     
-    let receive ?(queue_len=256) ?(timeout=None) receiver  = 
+        
+    (* let receive ?(queue_len=256) ?(timeout=None) receiver  = 
        let (self_source, self_sink) = EventStream.create queue_len in       
       let (run_loop, completer) = Lwt.task () in     
       let self = { 
@@ -71,22 +109,24 @@ module Actor = struct
           | `Terminate -> (Lwt_io.printf "Stop!" >|= fun _ -> (self, true)))
         | false -> Lwt.return (self, false)
       in Lwt.async (fun () -> loop (self, true))
-      ; self
-
-      
-
-    let (<!>) dest (from, message)  = EventStream.Sink.push (`ActorMessage (from,message)) dest.self_sink     
-    let send  dest from message = dest <!> (from, message) 
-         
-    let maybe_send dest from message = match dest with 
-      | Some actor -> actor <!> (from, message) >|= fun () -> true
-      | None -> Lwt.return false
-      
-    let (<?>) dest (from,message) = maybe_send dest from message
+      ; self *)
     
-    let run_loop self = self.run_loop
+    
+    let send  (dest : ([> ActorCoreMessage.core_message ] as 'msg) actor_mailbox) (from : 'msg actor_mailbox option ) (message : 'msg) = match dest with 
+      | ActorMailbox {inbox=_; outbox=outbox} -> EventStream.Sink.push  (from,message) outbox
 
-    let compare a b = ActorId.compare a.aid b.aid
-    let (=) a b = ActorId.(equal) a.aid b.aid 
+    let (<!>) dest (from, message)  = send dest from message      
+      
+    let maybe_send (dest : ([> ActorCoreMessage.core_message ] as 'msg) actor_mailbox option) (from : 'msg actor_mailbox option) (message: 'msg) = match dest with       
+      | Some actor -> (actor <!> (from, message)) >|= fun () -> true
+      | None -> Lwt.return false
+  
+    let (<?!>) dest (from,message) = maybe_send dest from message
+    
+
+    (* let compare a b = ActorId.compare a.aid b.aid
+    let (=) a b = ActorId.(equal) a.aid b.aid  *)
 
   end
+
+  
