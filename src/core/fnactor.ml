d0477793efa8f7415ddde59dd1f91460d2ec3717
Olivier Hecart <olivier.hecart@prismtech.com>
2018-07-24 17:39:31 +0200

    Improved fnactor

--- src/core/fnactor.ml
+++ src/core/fnactor.ml
@@ -4,16 +4,28 @@ exception Terminate
 
 let spawn ?(queue_len=256) ?(timeout=None) ?(on_terminate=None) state pack unpack = 
   let state = Some(pack (state)) in
+
+  let timeout = match timeout with 
+  | None -> None 
+  | Some (period, make_timeout) -> Some(period, fun fl -> (fun t -> unpack t |> make_timeout fl |> pack)) in
+
+  let on_terminate = match on_terminate with 
+  | None -> None 
+  | Some func -> Some(fun () -> (fun t -> unpack t |> func |> pack)) in
+
   let (mailbox, loop) = Actor.spawn ~queue_len ~state ~timeout ~on_terminate (fun self state _ f ->
     let state = Common.Option.get state in
     try Actor.continue self (Some (f state)) () 
-    with  | Terminate -> Actor.terminate self (Some state) ()
-          | e -> let msg = Printexc.to_string e
+    with  
+    | Terminate -> Actor.terminate self (Some state) ()
+    | e ->  let msg = Printexc.to_string e
             and stack = Printexc.get_backtrace () in
             Lwt.ignore_result @@ Logs_lwt.warn (fun m -> m "Fnactor got exception : %s\n%s" msg stack);
             Actor.continue self (Some state) ()
     ) in 
-  let push = fun func -> Actor.send mailbox None (fun state -> unpack state |> func  |> pack) in
+
+  let push = fun func -> Actor.send mailbox None (fun state -> unpack state |> func |> pack) in 
+  
   (push, loop)
 
 let terminate _ = raise Terminate 
--- src/core/fnactor.mli
+++ src/core/fnactor.mli
@@ -2,8 +2,8 @@ exception Terminate
 
 val spawn :
   ?queue_len:int ->
-  ?timeout:('b -> 'b) Actor.Actor.timeout_info option ->
-  ?on_terminate:(unit -> 'b -> 'b) option ->
+  ?timeout:('a -> 'a) Actor.Actor.timeout_info option ->
+  ?on_terminate:('a -> 'a) option ->
   'a -> ('a -> 'b) -> ('b -> 'a) -> 
   (('a -> 'a) -> unit Lwt.t) * unit Lwt.t
 
