6d905920f61c5ebad21310c369834162d655fde9
Angelo Corsaro <angelo@icorsaro.net>
2018-07-25 02:10:25 +0200

    used >>= instead of bind in state monad example

--- example/monad/statem.ml
+++ example/monad/statem.ml
@@ -8,15 +8,16 @@ open GameState.Infix
 
 let rec play_game = function 
 | [] -> read >>= (fun (_, score) -> return score)
-| (x::xs) -> let r = bind read (fun (on, score) -> 
-    match x with 
-    | 'a' when on -> write (on, score + 1)
-    | 'b' when on -> write(on, score - 1)
-    | 'c' -> write (not on, score)
-    | _ -> write (on, score)) in 
-     r >>= (fun _ -> play_game xs)
+| (x::xs) ->  
+    read 
+    >>= (fun (on, score) -> 
+        match x with 
+        | 'a' when on -> write (on, score + 1)
+        | 'b' when on -> write(on, score - 1)
+        | 'c' -> write (not on, score)
+        | _ -> write (on, score)) 
+        >>= (fun _ -> play_game xs)
   
-
 
 let () = 
   let r = eval (play_game ['a';'a';'c';'b';'c';'a';'a';'a';'a';'b']) (true, 0) in 
