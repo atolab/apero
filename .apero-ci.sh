8112ed5ced352ee4488a8a01a83b1441369961ac
gabrik <gabriele.baldoni@gmail.com>
2018-08-14 13:24:34 +0200

    update ci files

--- .apero-ci.sh
+++ .apero-ci.sh
@@ -1,9 +1,2 @@
-sh <(curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)
+sh .travis-opam.sh
 
-OPAM_DEPENDS="lwt dune logs fmt cmdliner"
-opam init 
-opam update
-opam upgrade
-opam install ${OPAM_DEPENDS}
-eval `opam config env`
-make
