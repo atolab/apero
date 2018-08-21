e9f3a816421d3d541783b4ce1468f33c8d7c0d14
Julien Enoch <946462+JEnoch@users.noreply.github.com>
2018-08-21 16:48:37 +0200

    Update Apero.Uuid to allow generation from a string

--- src/core/uuid.ml
+++ src/core/uuid.ml
@@ -1,19 +1,21 @@
 module Uuid = struct
   type t = Uuidm.t 
-  
+
   let state = Random.State.make_self_init ()
 
   let make = Uuidm.v4_gen state 
-    
-  let next_id = make 
-  
+
+  (* Note: ns_apero namespace is inspired by those specified in https://tools.ietf.org/html/rfc4122#appendix-C *)
+  let ns_apero = Uuidm.of_string "6ba7b842-9dad-11d1-80b4-00c04fd430c8" |> Common.Option.get
+  let make_from_string = Uuidm.v5 ns_apero
+
   let compare = Uuidm.compare
   let equal = Uuidm.equal
-  
+
   let of_bytes = Uuidm.of_bytes
   let to_bytes = Uuidm.to_bytes
-  
+
   let of_string = Uuidm.of_string
   let to_string = Uuidm.to_string
-  
+
 end 
--- src/core/uuid.mli
+++ src/core/uuid.mli
@@ -1,17 +1,17 @@
 module Uuid : sig  
   type t 
-  
-  val make : unit -> t  
-    
-  val next_id : unit -> t 
-  
+
+  val make : unit -> t
+
+  val make_from_string : string -> t
+
   val compare : t -> t -> int 
   val equal : t -> t -> bool
-  
+
   val of_bytes : ?pos:int -> string -> t option
   val to_bytes : t -> string
 
   val of_string : ?pos:int -> string -> t option
   val to_string : ?upper:bool -> t -> string
-  
+
 end 
